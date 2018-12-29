7a7411f794e4b88bdb3ddeb88c69737f1ca9472b
Mandala Akshay Kumar Goud <31163507+akshaytech9@users.noreply.github.com>
2018-12-30 00:22:00 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,25 +1,31 @@
+from flask import Flask, render_template
 from textblob import TextBlob
 import tweepy
 
-consumerKey = 'QBz9j6TvF7aEGWsLvVU2nxaGv'
-consumerSecret = '2jxrkIDox6DdktWloiUk9vQzv1Tty9dmz0BakivPgryB2fxUuI'
-accessToken = '379580689-xg4a9dT5PvVMqHgAjArs5N3s6WLLJnXS5IC2QjRB'
-accessTokenSecret = 'ZYYon4XxjE99gUnyx1xeVZ1NZIG5dhI8xc8y8I7Hr6GzR'
+app = Flask(__name__)
 
-auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
-auth.set_access_token(accessToken, accessTokenSecret)
+@app.route("/")
+def fun():
+	consumerKey = 'QBz9j6TvF7aEGWsLvVU2nxaGv'
+	consumerSecret = '2jxrkIDox6DdktWloiUk9vQzv1Tty9dmz0BakivPgryB2fxUuI'
+	accessToken = '379580689-xg4a9dT5PvVMqHgAjArs5N3s6WLLJnXS5IC2QjRB'
+	accessTokenSecret = 'ZYYon4XxjE99gUnyx1xeVZ1NZIG5dhI8xc8y8I7Hr6GzR'
 
-api = tweepy.API(auth)
+	auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
+	auth.set_access_token(accessToken, accessTokenSecret)
 
-tweets = api.search("machine learning")
+	api = tweepy.API(auth)
 
-for item in tweets:
-	tweet = TextBlob(item.text)
-	if tweet.sentiment.polarity > 0:
-		print("Positive Tweet")
-	elif tweet.sentiment.polarity < 0:
-		print("Negative Tweet")
-	else:
-		print("Neutral Tweet")
-	print("\n"+str(tweet))
-	print(" - - - - - - - - - - \n")
+	tweets = api.search("machine learning")
+	
+	t = dict()
+	
+	for item in tweets:
+		tweet = TextBlob(item.text)
+		t[tweet[:100]] = round(tweet.sentiment.polarity, 2)
+		
+	return render_template("sentiAnalyze.html", t_dict = t)
+	
+if __name__ == '__main__':
+	app.debug = True
+	app.run(host = '0.0.0.0', port=5000)
